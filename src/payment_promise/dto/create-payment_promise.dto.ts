a67450b047c7e0a5d8540bab4cd5558d3bf64538
abraham <code.arb2712@gmail.com>
2026-01-13 11:17:42 -0400

    add @ApiProperty for documentation

--- src/payment_promise/dto/create-payment_promise.dto.ts
+++ src/payment_promise/dto/create-payment_promise.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID, IsDateString, IsNotEmpty } from 'class-validator';
 
 export class CreatePaymentPromiseDto {
+  @ApiProperty({ description: 'fecha de validacion', example: '12/01/2026' })
   @IsDateString()
   @IsNotEmpty()
   valid_until: string;
 
+  @ApiProperty({ description: 'id del contrato', example: '13123-d1212-dsfs1' })
   @IsUUID()
   @IsNotEmpty()
   contract_id: string;
@@ -13,14 +16,23 @@ export class CreatePaymentPromiseDto {
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'monto pagado', example: '200.00' })
   @IsString()
   @IsNotEmpty()
   amount: string;
 
+  @ApiProperty({
+    description: 'banco al que se realizo la transferencia',
+    example: 'Venezuela',
+  })
   @IsString()
   @IsNotEmpty()
   bank: string;
 
+  @ApiProperty({
+    description: 'Codigo de referencia bancaria',
+    example: '123456',
+  })
   @IsString()
   @IsNotEmpty()
   transaction_code: string;
